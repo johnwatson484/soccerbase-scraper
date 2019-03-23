be3280b7aaafcc3292274efe05a7ff26e3eae340
John Watson <johnwatson484@hotmail.com>
2019-03-23 19:07:27 +0000

    Initial

---
+++ SoccerbaseScraper/Classes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerbaseScraper.Classes
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Position { get; set; }
+
+        public string Team { get; set; }
+    }
+
+}
